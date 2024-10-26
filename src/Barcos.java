24a3c1b64160da1450f4997fe2231147cf93066a
JesusM15 <guerrajesusm72@gmail.com>
2024-10-25 18:58:39 -0700

    estructura base

---
+++ src/Barcos.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Barcos {
+    ArrayList<Barco> barcos;
+
+    public Barcos() {
+
+    }
+
+    public void generarBarcos() {
+
+    }
+}
---
+++ src/Oceano.java
@@ -0,0 +1,2 @@
+public class Oceano {
+}
